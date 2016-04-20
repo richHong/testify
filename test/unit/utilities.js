3d93e5d9d0d197fc2c16b4a851ffd062c234e3b1
richHong <rich.hong@gmail.com>
2016-04-20 12:27:14 -0700

    Almost through writing tests for todo.util

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -5,10 +5,35 @@ var expect = chai.expect;
 describe('the todo.App', function() {
   describe('the todo object', function(){
 
+
     it('should have all the necessary methods', function(){
+      expect(todo.util.trimTodoName).to.be.a('function');
+      expect(todo.util.isValidTodoName).to.be.a('function');
+      expect(todo.util.getUniqueId).to.be.a('function');
     });
   });
 });
 
 describe('the todo.util methods', function() {
+  
+  it('should trim the whitespace from the TodoName', function() {
+    var name = "Richard     ";
+    var answer = todo.util.trimTodoName(name);
+    expect(answer).to.equal("Richard");
+  });
+
+  it('should validate the name', function() {
+    var wrongName = 'R';
+    var answer = todo.util.isValidTodoName(wrongName);
+    expect(answer).to.equal(false);
+  });
+
+  it('should get a unique ID', function() {
+    var lastId = 1;
+    console.log('lastId',lastId);
+    todo.util.getUniqueId();
+    console.log('lastId after answer',lastId);
+    expect(lastId).to.not.equal(1);
+  });
+
 });
